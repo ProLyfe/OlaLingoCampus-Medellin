0e00c69f101cfdcbad12c3911375388cfbbd5a1d
Prolyfe <rayane.prolyfe@gmail.com>
2024-10-18 10:10:32 -0500

    [fix] spanish test

--- src/components/pages/landing/language-journey/language-journey-data.ts
+++ src/components/pages/landing/language-journey/language-journey-data.ts
@@ -1,6 +1,6 @@
 export const languageJourneyData = {
   teacher: {
-    title: "Únete a nosotras como profesora hoy",
+    title: "Únete con nosotros como profesor hoy",
     content: "Forma parte de nuestra red global de profesores de idiomas. Regístrate ahora y empieza a inspirar a los estudiantes mientras desarrollas tu carrera docente",
   },
   student: {
